62909b14128fa023a5a18885642f7f53a892be2c
CHRISTIAN GRANT <tcpip001@gmail.com>
2017-10-19 07:27:44 +0200

    updated comments in waypoint_updater.py

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -19,6 +19,7 @@ current status in `/vehicle/traffic_lights` message. You can use this message to
 as well as to verify your TL classifier.
 
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
+
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
